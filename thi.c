0187e790020e966e830ba0752df3d726f4b26728
refanr <reynirg20@ru.is>
2022-05-24 23:11:22 +0000

    figured out I function

--- thi.c
+++ thi.c
@@ -6,8 +6,8 @@
 #include <time.h>
 #include <pthread.h>
 
-#define SIZE 8
-#define CORES 1
+#define SIZE 16
+#define CORES 2
 #define F(x,y,z) ((x & y) | (~x & z))
 #define G(x,y,z) ((x & z) | (y & ~z))
 #define H(x,y,z) (x ^ y ^ z)
@@ -23,21 +23,22 @@ unsigned int output[0xfffff] = {0};
 
 void* threadFunc(void *index)
 {
-    printf("threadFunc\n");
-    unsigned int *i = (unsigned int *)(index);
+    unsigned int *i = (unsigned int *)(index);    
     unsigned int min,max;
-    min = (*i) * CHUNK;
+    min = ((*i)-1) * CHUNK;
     max = min + CHUNK;
-
+    
+    
     for(;min < max;min++)
-    {     
+    {
+             
         for (unsigned int j=0;j<SIZE;j++)
         {
+            
             for (unsigned int k=0;k<SIZE;k++)
             {
-                unsigned int idx = iFunc(min,j,k);
-                printf("i: %d, j: %d, k: %d, idx: %d\n", *i,j,k, idx);
-                //output[idx] += 1;
+                unsigned int idx = F(min,j,k);
+                output[idx] += 1;
             }
         }
     }
@@ -53,20 +54,24 @@ int main()
     
     pthread_t tid[CORES];
 
-    for (int i=0;i<CORES;i++)
+    for (unsigned int i=0;i<CORES;i++)
     {
-        printf("threadloop\n");
+        printf("threadloop i: %d\n",i);
         pthread_create (&tid[i], NULL, threadFunc, (void *)(&i));
     }
         
     
     for (int i=0; i<CORES;i++)
         pthread_join(tid[i],NULL);
-
+    
     end = clock();
-    long t = end - start; 
-
-    //for (int i=0; i<SIZE; i++) { printf("%d - %d\n", i, output[i]); } 
+    long t = end - start;
+    int i = 0;
+     
+    // for I function:
+    //for (; i<SIZE; i++) { printf("%d - %d\n", (~i+((int)pow(2.0,32.0))), output[i]); }
+    // for F,G,H functions: 
+    for (; i<SIZE; i++) { printf("%d - %d\n", i, output[i]); } 
        
     printf("Time for SIZE(%d), CORES(%d), CHUNK(%d) -> %ld \xC2\xB5s\n", SIZE, CORES, CHUNK, t);
 
