4c7bb662a94859a55b466fdaf25f02a7db17d0ad
refanr <reynirg20@ru.is>
2022-05-25 01:03:31 +0000

    last edit

--- allFunctions.c
+++ allFunctions.c
@@ -131,7 +131,7 @@ int main()
     for (int i=0; i<CORES;i++)
         pthread_join(tid[i],NULL);
     
-    FILE *out_file = fopen("output/testF.txt", "w");
+    FILE *out_file = fopen("output/Ffunction.txt", "w");
 
     if (out_file == NULL)
     {
@@ -151,7 +151,7 @@ int main()
     for (int i=0; i<CORES;i++)
         pthread_join(tid[i],NULL);
 
-    FILE *out_fileG = fopen("output/testG.txt", "w");
+    FILE *out_fileG = fopen("output/Gfunction.txt", "w");
 
     if (out_fileG == NULL)
     {
@@ -171,7 +171,7 @@ int main()
     for (int i=0; i<CORES;i++)
         pthread_join(tid[i],NULL);
 
-    FILE *out_fileH = fopen("output/testH.txt", "w");
+    FILE *out_fileH = fopen("output/Hfunction.txt", "w");
 
     if (out_fileH == NULL)
     {
@@ -191,7 +191,7 @@ int main()
     for (int i=0; i<CORES;i++)
         pthread_join(tid[i],NULL);
     
-    FILE *out_fileI = fopen("output/testI.txt", "w");
+    FILE *out_fileI = fopen("output/Ifunction.txt", "w");
 
     if (out_fileI == NULL)
     {
@@ -212,7 +212,7 @@ int main()
     //for (; i<SIZE; i++) { printf("%d - %d\n", i, output[i]); }
 
     
-    // FILE *out_file = fopen("output/testF.txt", "w");
+    // FILE *out_file = fopen("output/Ffunction.txt", "w");
 
     // if (out_file == NULL)
     // {
